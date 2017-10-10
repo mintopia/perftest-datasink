5d01f1497874d5a09474445f27a8cbb137cd43c3
Jessica Smith <jessica.smith@fasthosts.com>
2017-10-10 10:31:17 +0100

    Update styling

--- src/public/index.php
+++ src/public/index.php
@@ -8,6 +8,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <style>
+            h1 {
+                margin-bottom: 4rem;
+            }
+        </style>
   </head>
   <body>
   	<div class="container">
