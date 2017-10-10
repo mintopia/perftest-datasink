437fb9ded1f80d932c41045595b5f15d1a857d57
Jessica Smith <jessica.smith@fasthosts.com>
2017-10-10 09:56:51 +0100

    Add source to output

--- src/public/sink/index.php
+++ src/public/sink/index.php
@@ -1,13 +1,17 @@
 <?php
 	$bytes = 0;
+	$source = '';
 	if (isset($_POST['data'])) {
 		$bytes = strlen($_POST['data']);
+		$source = 'POST';
 	}
 	if (isset($_GET['data'])) {
 		$bytes = strlen($_GET['data']);
+		$source = 'GET';
 	}
 	if (isset($_FILES['data'])) {
 		$bytes = $_FILES['data']['size'];
+		$source = 'FILE';
 	}
 	header('Content-Type: application/json');
 	echo json_encode((object) [
